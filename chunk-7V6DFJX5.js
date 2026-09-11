eecbb74bd8dd28758682e3140e76af33fb8ea897
saravanansaranraj27 <ssaranraj15102021@gmail.com>
2026-09-12 00:15:52 +0530

    Auto-generated commit

---
+++ chunk-7V6DFJX5.js
@@ -0,0 +1,1 @@
+import{Ja as e}from"./chunk-ELLL54BA.js";var i=class t{transform(r){return r?r.charAt(0).toUpperCase()+r.slice(1):""}static \u0275fac=function(p){return new(p||t)};static \u0275pipe=e({name:"capitalize",type:t,pure:!0})};export{i as a};
